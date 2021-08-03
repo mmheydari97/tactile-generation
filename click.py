4573e8db8932926f9c3458e57d9f05d50403489c
mmheydari97 <heidarymm@yahoo.com>
2021-08-03 02:14:56 -0400

    gen data separated

---
+++ click.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+CROSS = []
+def on_press(event):
+    # print("my position:" ,event.button,event.xdata, event.ydata)
+    
+    if str(event.button) == "MouseButton.LEFT":
+        global CROSS
+        CROSS.append([event.xdata, event.ydata])
+    
+
+file_count = len(next(os.walk("./points"))[2])
+
+for i in range(file_count):
+    b = np.load(f"points/{i+1}.npy")
+
+
+    fig = plt.figure()
+    plt.plot(b[:,0], b[:,1])
+    fig.canvas.mpl_connect('button_press_event', on_press)
+    plt.show()
+
+    np.save(f"./intersections/{i+1}.npy", np.array(CROSS))
---
+++ data_gen.py
@@ -0,0 +1,28 @@
+import random
+import numpy as np
+from math import comb
+
+P_1D = 0.25
+CROSS = []
+
+def bezier(x, P):
+    n = P.shape[0]
+    b = 0
+
+    for i in range(n):
+        a = comb(n,i)*((1-x)**(n-i))*(x**i)
+        b = b + a*P[i].reshape(1,-1)
+
+    if b.shape[1] == 1:
+        b = np.concatenate((x,b), axis=1)
+    return b
+
+x = np.linspace(0, 1, 10000).reshape(-1,1)
+P = np.array([[random.randint(-20,20), random.randint(-20,20)] for i in range(random.randint(2,20))])
+
+if random.random() <= P_1D:
+    P = P.reshape(-1,1).flatten()[::2]
+
+for i in range(10):
+    b = bezier(x, P)
+    np.save(f"./points/{i+1}.npy", b)
