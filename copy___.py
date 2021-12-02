7689fd8a2bb5867c35035384e3de4671a90148c3
mmheydari97 <heidarymm@yahoo.com>
2021-12-02 18:28:09 -0500

    edit conflicting file name

--- copy___.py
+++ copy___.py
@@ -4,7 +4,7 @@ from pathlib import Path
 with open("./with_intersection.txt") as f:
     for id in f.readlines():
         id = id.split('\n')[0]
-        src = Path(f"./source/s_{id}.jpg")
-        trg = Path(f"./tactile/t_{id}.jpg")
+        src = Path(f"./source_cv/s_{id}.jpg")
+        trg = Path(f"./mask/s_{id}.jpg")
         copy2(src, f"../../../New/source")
-        copy2(trg, f"../../../New/tactile")
+        copy2(trg, f"../../../New/mask")
