13c02561456d0f5cbec2bbd5daf99a4a0e1790e6
mmheydari97 <heidarymm@yahoo.com>
2021-12-02 19:46:32 -0500

    grids mismatch fixed

--- draw.py
+++ draw.py
@@ -11,9 +11,9 @@ file_count = len(next(os.walk("./intersections"))[2])
 np_load_old = np.load
 np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
 
-def draw_grids(ax):
+def draw_grids(ax, **kwargs):
     ax.set_zorder(0)
-    plt.grid(which='both', axis='both')
+    plt.grid(which='both', axis='both', **kwargs)
     
 for i in range(file_count):
     PLOT_COLOR = "#"+''.join([random.choice('0123456789abcdef') for j in range(6)])
@@ -21,7 +21,10 @@ for i in range(file_count):
     FIG_SIZE = random.choices([[10,10], [10,20], [20,10]], weights=[.5, .25, .25])[0]
     b = np.load(f"./points/{i+1}.npy")
     cross = np.load(f"./intersections/{i+1}.npy")
-    with plt.style.context(random.choices(['classic', 'dark_background'], weights=[.85, .15])[0]):
+
+    # with plt.style.context(random.choices(['default', 'dark_background'], weights=[.85, .15])[0]):
+    with plt.style.context('default'):
+    
         fig1 = plt.figure(figsize=FIG_SIZE)
         ax1 = plt.gca()
         ax1.set_xticklabels('')
@@ -44,7 +47,7 @@ for i in range(file_count):
         ax2.tick_params(direction='inout', length=20, width=2)
         plt.plot(b[:,0], b[:,1], zorder=1, c=PLOT_COLOR, lw=5)
         if GRID_PARAM < 0.40:
-            draw_grids(ax2)
+            draw_grids(ax2, color='k', linestyle='--', linewidth=2)
         
         if cross.size != 0:
             scolor = "#{:06X}".format(16777215 - int(PLOT_COLOR.split("#")[1], 16))
@@ -52,31 +55,32 @@ for i in range(file_count):
         plt.savefig(f'./tactile/t_{i+1}.jpg')
 
 
-    # with plt.style.context('default'):
-    #     fig3 = plt.figure(figsize=FIG_SIZE)
-    #     ax3 = plt.gca()
-    #     ax3.set_xticklabels('')
-    #     ax3.set_yticklabels('')
-    #     ax3.axes.xaxis.set_visible(False)
-    #     ax3.axes.yaxis.set_visible(False)
+    with plt.style.context('default'):
+        fig3 = plt.figure(figsize=FIG_SIZE)
+        ax3 = plt.gca()
+        ax3.set_xticklabels('')
+        ax3.set_yticklabels('')
+        ax3.axes.xaxis.set_visible(False)
+        ax3.axes.yaxis.set_visible(False)
 
-    #     for l in ax3.spines.values():
-    #         l.set_visible(False)
-    #     plt.plot(b[:,0], b[:,1], zorder=-1, c='w')
-    #     if cross.size != 0:
-    #         plt.scatter(cross[:,0], cross[:,1], zorder=1, s=3, c='k')
+        for l in ax3.spines.values():
+            l.set_visible(False)
+        plt.plot(b[:,0], b[:,1], zorder=-1, c='w')
+        if cross.size != 0:
+            plt.scatter(cross[:,0], cross[:,1], zorder=1, s=3, c='k')
 
-    #     plt.savefig(f'./mask/t_{i+1}.jpg')
+        plt.savefig(f'./mask/t_{i+1}.jpg')
     
     
     
-    # with Image.open(f'./mask/t_{i+1}.jpg') as img:
-    #     img = invert(expand2square(img))
-    #     img = ImageOps.equalize(img, mask=None)
-    #     img = img.filter(ImageFilter.GaussianBlur(radius=2))
-    #     img.save(f'./mask/t_{i+1}.jpg')
+    with Image.open(f'./mask/t_{i+1}.jpg') as img:
+        img = invert(expand2square(img))
+        img = ImageOps.equalize(img, mask=None)
+        img = img.filter(ImageFilter.GaussianBlur(radius=2))
+        img.save(f'./mask/t_{i+1}.jpg')
 
     plt.close('all')
+    print(f"sample {i+1} drawn")
 
 # with open("./with_intersection.txt","w") as f:
 #     for i in range(10000):
